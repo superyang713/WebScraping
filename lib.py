5d8c9212f8b7a112b06eb19fd8caedcd382496e1
Yang Dai <yangdai713@aim.com>
2018-05-04 16:17:32 -0400

    add error handling

--- lib.py
+++ lib.py
@@ -1,6 +1,7 @@
 import requests
 from requests.exceptions import HTTPError, ConnectionError
 from bs4 import BeautifulSoup
+import re
 
 
 def getTitle(url):
@@ -9,7 +10,10 @@ def getTitle(url):
         r.raise_for_status()
     except HTTPError as e:     # Page not exist
         return None
-    except ConnectionError:    # bad url
+    except ConnectionError:    # bad url, or no internet connection
+        return None
+    except Exception:
+        print('Something went wrong!')
         return None
 
     try:
@@ -17,4 +21,32 @@ def getTitle(url):
         title = bs.body.h1
     except AttributeError as e:
         return None
+    except Exception:
+        print('Something went wrong!')
     return title
+
+
+def wiki_get_links(article_url):
+    """It finds all the article urls inside a wiki article page."""
+    try:
+        r = requests.get('http://en.wikipedia.org{}'.format(article_url))
+        r.raise_for_status()
+    except HTTPError:
+        print('The page does not exist!')
+        return None
+    except ConnectionError:
+        print('Bad link, or check your internet connection!')
+        return None
+
+    try:
+        bs = BeautifulSoup(r.text, 'html.parser')
+        articles = bs.find('div', {'id': 'bodyContent'}).find_all(
+            'a', {'href': re.compile('^(/wiki/)((?!:).)*$')}
+        )
+        return articles
+    except AttributeError:
+        print('Cannot find the content!')
+        return None
+    except Exception:
+        print('Something went wrong!')
+        return None
--- wiki_sixdegree.py
+++ wiki_sixdegree.py
@@ -4,31 +4,23 @@ in the intial page. Then, it goes into a loop, finding a random article link tag
 in the page, extracting the href attribute from it, printing the page, and
 getting a new list of links from the extracted url.
 """
-import requests
-from bs4 import BeautifulSoup
-import datetime
+from lib import wiki_get_links
 import random
-import re
+import datetime
 
 
 random.seed(datetime.datetime.now())
 
-# TODO: needs to add exceptions to make the program more robust.
-
 
 def main():
-    links = get_links('/wiki/Spectroscopy')
-    while len(links) > 0:
-        new_article = random.choice(links).attrs['href']
-        print(new_article)
-        links = get_links(new_article)
-
-
-def get_links(article_url):
-    r = requests.get('http://en.wikipedia.org{}'.format(article_url))
-    bs = BeautifulSoup(r.text, 'html.parser')
-    return bs.find('div', {'id': 'bodyContent'}).find_all(
-        'a', {'href': re.compile('^(/wiki/)((?!:).)*$')})
+    links = wiki_get_links('/wiki/donut')
+    try:
+        while len(links) > 0:
+            new_article = random.choice(links).attrs['href']
+            print(new_article)
+            links = wiki_get_links(new_article)
+    except TypeError as e:
+        return None
 
 
 if __name__ == '__main__':
